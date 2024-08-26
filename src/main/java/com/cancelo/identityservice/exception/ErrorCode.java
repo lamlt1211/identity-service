67e6091b858678ecf73068f2c4d647c0c316d9e0
lamlt1211 <lamchuot0@gmail.com>
2024-08-26 11:56:15 +0700

    exception

--- src/main/java/com/cancelo/identityservice/exception/ErrorCode.java
+++ src/main/java/com/cancelo/identityservice/exception/ErrorCode.java
@@ -1,10 +1,11 @@
 package com.cancelo.identityservice.exception;
 
 public enum ErrorCode {
-    USER_EXISTED(1002, "User existed");
-
-
-
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized exception"),
+    INVALID_KEY(1001,"Invalid key"),
+    USER_EXISTED(1002, "User existed"),
+    USERNAME_INVALID(1003, "Username must be at least 3 characters"),
+    INVALID_PASSWORD(1004, "Password must be at least 6 characters"),;
 
     ErrorCode(int code, String message) {
         this.code = code;
--- src/main/java/com/cancelo/identityservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cancelo/identityservice/exception/GlobalExceptionHandler.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = RuntimeException.class)
+    @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception) {
         ApiResponse apiResponse = new ApiResponse();
 
-        apiResponse.setCode(1001);
-        apiResponse.setMessage(exception.getMessage());
+        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
     }
@@ -24,14 +24,30 @@ public class GlobalExceptionHandler {
         ErrorCode errorCode = exception.getErrorCode();
         ApiResponse apiResponse = new ApiResponse();
 
-        apiResponse.setCode(errorCode);
+        apiResponse.setCode(errorCode.getCode());
         apiResponse.setMessage(exception.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<String> handlingMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest().body(e.getFieldError().getDefaultMessage());
+    ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException exception) {
+        String enumKey = exception.getFieldError().getDefaultMessage();
+
+
+        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        ApiResponse apiResponse = new ApiResponse();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(exception.getMessage());
+
+        return ResponseEntity.badRequest().body(apiResponse);
     }
 }
